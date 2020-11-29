d98963a51a98327e021b381666f9943ae3292aa4
Raquel Magnani <quel.magnani88@gmail.com>
2020-11-29 13:44:03 -0300

    aula-07

---
+++ aula-07/Erro02.java
@@ -0,0 +1,6 @@
+class Erro02 {
+	public static void main(String[] args){
+		int cont;
+	System.out.println("Voce digitou: " + cont);
+	}
+}
---
+++ aula-07/Erro03.java
@@ -0,0 +1,7 @@
+class Erro03 {
+	//o metodo main com M maisculo, não é o metodo de entrada do java... é uma
+	//classe java por isso compila... mas ta errado... o correto é main !
+	public static void Main(String[] args){
+	System.out.println("Hello World");
+	}
+}
