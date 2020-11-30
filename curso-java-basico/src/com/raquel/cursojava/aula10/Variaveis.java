3c7be68f7435803bf9cf1203dd1f0bbbf831ed44
Raquel Magnani <quel.magnani88@gmail.com>
2020-11-30 14:29:23 -0300

    aula 10

---
+++ curso-java-basico/src/com/raquel/cursojava/aula10/Variaveis.java
@@ -0,0 +1,18 @@
+package com.raquel.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		String nome = "Raquel";
+		String nomeDoMeuCachorro = "Pingo";
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("nome = " + nomeDoMeuCachorro);
+
+	}
+
+}
